bfc99ef9c687a7eaedd90dc7f8689cf1f4138e66
webhenrique <henrique-alves@netcabo.pt>
2020-03-24 12:09:53 +0000

    includejs

---
+++ js/include.js
@@ -0,0 +1,55 @@
+fetch("include/portfolio.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('portfolio').innerHTML = data;
+    });
+
+    fetch("include/intro.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('intro').innerHTML = data;
+    });
+
+    fetch("include/about.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('about').innerHTML = data;
+    });
+
+    fetch("include/contact.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('contact').innerHTML = data;
+    });
+
+    fetch("include/services.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('services').innerHTML = data;
+    });
+
+    fetch("include/team.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('team').innerHTML = data;
+    });
+
+    fetch("include/why-us.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('why-us').innerHTML = data;
+    });
